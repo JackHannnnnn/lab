af7443cb5404c4d6bf366f1d269396bb87cf8545
chaha@microsoft.com <fange1206@gmail.com>
2020-05-30 16:56:13 -0700

    Added more comments and support %o display

--- inc/elf.h
+++ inc/elf.h
@@ -10,12 +10,12 @@ struct Elf {
 	uint16_t e_machine;
 	uint32_t e_version;
 	uint32_t e_entry;
-	uint32_t e_phoff;
+	uint32_t e_phoff;  // offset in the disk of first program section
 	uint32_t e_shoff;
 	uint32_t e_flags;
 	uint16_t e_ehsize;
 	uint16_t e_phentsize;
-	uint16_t e_phnum;
+	uint16_t e_phnum;  // num of program sections in this ELF
 	uint16_t e_shentsize;
 	uint16_t e_shnum;
 	uint16_t e_shstrndx;
@@ -23,11 +23,11 @@ struct Elf {
 
 struct Proghdr {
 	uint32_t p_type;
-	uint32_t p_offset;
+	uint32_t p_offset; // offset in the disk space
 	uint32_t p_va;
-	uint32_t p_pa;
+	uint32_t p_pa;  // target load physical address
 	uint32_t p_filesz;
-	uint32_t p_memsz;
+	uint32_t p_memsz; // program section size
 	uint32_t p_flags;
 	uint32_t p_align;
 };
