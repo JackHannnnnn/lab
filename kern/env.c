12319ba5afc108d4a5ecae1c80075401faeade78
chaha@microsoft.com <fange1206@gmail.com>
2021-06-20 18:04:21 -0700

    Fixed the curenv null pointer error

--- kern/env.c
+++ kern/env.c
@@ -554,7 +554,7 @@ env_run(struct Env *e)
 
 	// LAB 3: Your code here.
 	if (e != curenv) {
-		if (curenv->env_status == ENV_RUNNING) {
+		if (curenv && curenv->env_status == ENV_RUNNING) {
 			curenv->env_status = ENV_RUNNABLE;
 		}
 		curenv = e;
